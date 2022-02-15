fcc1dbd5efa67d476e89832d097eba9bebf6c094
Sathviek <71971661+Sathviek@users.noreply.github.com>
2022-02-15 17:57:36 +0530

    Add files via upload

---
+++ RemoveFiles.py
@@ -0,0 +1,21 @@
+import time,shutil,os
+
+path=input("enter the name of the directory:")
+days=input("file creation day:")
+
+
+def main():
+    deleted_folders_count=0
+    deleted_files_count=0
+
+    path="/PATH_TO_DELETE"
+    days=30
+
+    seconds=time.time()-(days*24*60*60)
+    os.path.exists()
+    os.walk()
+    os.path.join()
+    os.stat()
+    os.st_ctime(>=30)
+    os.remove()
+    shutil.rmtree()
